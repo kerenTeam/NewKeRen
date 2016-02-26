74d7d0d80b97bcc7e462a16b062b773a1cbeaefa
774133886 <774133886@qq.com>
2016-02-26 17:32:12 +0800

    index 入口文件修改

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'home/';
+	$application_folder = 'home';
 
 /*
  *---------------------------------------------------------------
